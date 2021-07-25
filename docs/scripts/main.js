076357f0994f27be02b3553d3f90f2d8f26d4508
Hyunjoon <chunuiyu@gmail.com>
2021-07-25 19:47:15 +0900

    first commit for Hyunjoon & Eunmin

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,79 +1,151 @@
 // Add your javascript here
 // Don't forget to add it into respective layouts where this js file is needed
+d = new Date();
 $(document).ready(function () {
-    $("#map-image").on("click")
-    {
+    $(".grm_account_bank").html("KB국민");
+    $(".brd_account_bank").html("NH농협");
+    $(".grm_account_num").text("208401 04 273636");
+    $(".brd_account_num").text("312 0204 6512 61");
+    $(".GroomAccountName").text("박종국");
+    $(".BrideAccountName").text("임헌호");
+    $(".GroomAccountRelation").text("혼주");
+    $(".BrideAccountRelation").text("혼주");
 
+    $(".grm_account_bank2").html("NH농협");
+    $(".brd_account_bank2").html("우리");
+    $(".grm_account_num2").text("811091 52 0822 56");
+    $(".brd_account_num2").text("1002 741 237704");
+    $(".GroomAccountName2").text("박현준");
+    $(".BrideAccountName2").text("임은민");
+    $(".GroomAccountRelation2").text("신랑");
+    $(".BrideAccountRelation2").text("신부");
+
+
+    if ($.trim("혼주") != "") {
+        $(".GroomAccountRelationArea").show();
+    } else {
+        $(".GroomAccountRelationArea").hide();
     }
 
-    $('#go-to-top').click(function () {
-        $('html,body').animate({scrollTop: 0}, 400);
-        return false;
-    });
+    if ($.trim("혼주") != "") {
+        $(".GroomAccountRelationArea2").show();
+    } else {
+        $(".GroomAccountRelationArea2").hide();
+    }
 
-    $(".gift-send").click(function () {
-        $("#gift-name").text($(this).data("name"));
-    })
+    if ($.trim("혼주") != "") {
+        $(".BrideAccountRelationArea").show();
+    } else {
+        $(".BrideAccountRelationArea").hide();
+    }
+
+    if ($.trim("신부") != "") {
+        $(".BrideAccountRelationArea2").show();
+    } else {
+        $(".BrideAccountRelationArea2").hide();
+    }
 
 
-    $("#reserveGiftButton").click(function () {
-        let name = $("#sender-name").val();
-        let message = $("#sender-message").val();
-        $("#reserveGiftButton").text("전송중...");
-        $("#reserveGiftButton").prop("disabled", true);
+    if ($.trim("박종국") != "") {
+        $(".GroomAccountNameArea").show();
+    } else {
+        $(".GroomAccountNameArea").hide();
+    }
 
-        emailjs.init("user_yjLL5xG0A3kkOCH5BGIDh");
-        emailjs.send("wedding-mail", "gift_send", {
-            name: name,
-            gift: $("#gift-name").text(),
-            message: message
-        }).then(function (response) {
-            $('#giftMailModal').modal('hide');
-            alert(name + "님의 메시지가 정상적으로 전송되었습니다.");
+    if ($.trim("박현준") != "") {
+        $(".GroomAccountNameArea2").show();
+    } else {
+        $(".GroomAccountNameArea2").hide();
+    }
 
-            $("#reserveGiftButton").text("예약하기!");
-            $("#sender-name").val('');
-            $("#sender-message").val('');
-            $("#reserveGiftButton").prop("disabled", false);
-        }, function (err) {
-            alert("메시지 전송이 실패했습니다. 다시 시도해주세요.");
-        });
-    })
-})
+    if ($.trim("임헌호") != "") {
+        $(".BrideAccountNameArea").show();
+    } else {
+        $(".BrideAccountNameArea").hide();
+    }
 
-// Smooth scroll for links with hashes
-$("a.smooth-scroll").click(function (event) {
-    // On-page links
-    if (
-        location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
-        location.hostname == this.hostname
-    ) {
-        // Figure out element to scroll to
-        var target = $(this.hash);
-        target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
-        // Does a scroll target exist?
-        if (target.length) {
-            // Only prevent default if animation is actually gonna happen
-            event.preventDefault();
-            $("html, body").animate(
-                {
-                    scrollTop: target.offset().top
-                },
-                1000,
-                function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) {
-                        // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    }
-                }
-            );
+    if ($.trim("임은민") != "") {
+        $(".BrideAccountNameArea2").show();
+    } else {
+        $(".BrideAccountNameArea2").hide();
+    }
+
+
+    if ("Y" == "Y" || "Y" == "Y" || "Y" == "Y" || "Y" == "Y") {
+        $(".remittance").show();
+        $("#remittance").show();
+    } else {
+        $(".remittance").hide();
+        $("#remittance").hide();
+    }
+
+    if ("Y" == "Y" || "Y" == "Y") {
+        $(".btn_bx_1").show();
+        $("#btn_bx_1").show();
+
+        if ("Y" == "Y") {
+            $(".re_grm_1").show();
+        } else {
+            $(".re_grm_1").hide();
         }
+        if ("Y" == "Y") {
+            $(".re_grm_2").show();
+        } else {
+            $(".re_grm_2").hide();
+        }
+    } else {
+        $(".btn_bx_1").hide();
+        $("#btn_bx_1").hide();
+    }
+
+    if ("Y" == "Y" || "Y" == "Y") {
+        $(".btn_bx_2").show();
+        $("#btn_bx_2").show();
+
+        if ("Y" == "Y") {
+            $(".re_brd_1").show();
+        } else {
+            $(".re_brd_1").hide();
+        }
+        if ("Y" == "Y") {
+            $(".re_brd_2").show();
+        } else {
+            $(".re_brd_2").hide();
+        }
+
+    } else {
+        $(".btn_bx_2").hide();
+        $("#btn_bx_2").hide();
     }
 });
+
+//초기화
+$('#remittance_pop_h').hide();
+$('#remittance_pop_w').hide();
+
+// 팝업
+$(function () {
+    $('.h_btn').click(function () {
+        $('#remittance_pop_h').show();
+        //$('.remittance_pop_h').show();
+    });
+    $('.w_btn').click(function () {
+        $('#remittance_pop_w').show();
+        //$('.remittance_pop_w').show();
+    });
+    $('.close_btn').click(function () {
+        $('#remittance_pop_h, #remittance_pop_w').hide();
+        //$('.remittance_pop_h, .remittance_pop_w').hide();
+    });
+});
+
+//복사
+function copyToClipboard(elementId) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", document.getElementById(elementId).innerHTML);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    alert("복사되었습니다.");
+}
